b62a65cff943fa4588ca1d20d95c43b8296e763d
mauridb <mauriziocontatto@gmail.com>
2017-10-15 12:40:58 +0200

    add README.md, add app backend to settings file and create a simple data model for the platform based on auth.User, Project, Skill

---
+++ README.md
@@ -0,0 +1,27 @@
+# Documentation
+
+This project born to be a support and a storage of my own development learnt 'skills' during hard development session at the week. This platform allow should allow add and descript our learnt 'things' stored in a SQL db.
+
+## Requirements
+```
+Docker
+docker-compose
+```
+
+## Installation
+Keep it simple, in root project directory launch as root
+```bash
+$ docker-compose build
+```
+
+**Run application as root:**
+```bash
+$ docker-compose up
+```
+*Visit* [localhost:8000]
+
+and follow logs!Cool, It's working!
+
+*"Lovely developed <3 by mauridb!"*
+
+
---
+++ backend/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=256, blank=False, null=False)
+    project_description = models.TextField()
+    project_status = models.CharField(max_length=10) # TODO: enum field
+    project_likes = models.IntegerField()
+    project_level = models.CharField(max_length=10) # TODO: enum field
+    author = models.ForeignKey(User)
+
+class Skill(models.Model):
+    skill_name = models.CharField(max_length=55, blank=False, null=False)
+    skill_status = models.CharField(max_length=10)
+    skill_description = models.TextField()
+    skill_type = models.CharField(max_length=10) # TODO: better if enum type field BE|FE allow null
+    skill_area = models.CharField(max_length=10) # TODO: better if enum type field not null VCS|DOCKER|SCRIPTING|VIEW ...
+    skill_level = models.CharField(max_length=10) # TODO: better if enum type field BASIC|INTERMEDIATE|ADVANCED|MASTER|DOMAIN
+    skill_value = models.DecimalField(max_digits=3, decimal_places=1)
+    skill_likes = models.IntegerField()
+    project = models.ForeignKey(Project)
+    owner = models.ForeignKey(User)

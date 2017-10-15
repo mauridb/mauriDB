c2ea7c7ac81f1623539c34816c0b7bc5ba4c74e0
mauridb <mauriziocontatto@gmail.com>
2017-10-15 13:57:45 +0200

    set up templates file.html, a litlle app routing system. PREPARED frontend development and APIREST design, LET'S GO

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# application routing
+def home(request):
+    context = {
+        'msg': 'django html'
+    }
+    return render(request, 'home.html', context)
+
+
+def login(request):
+    context = {
+        'msg': 'login html'
+    }
+    return render(request, 'login.html', context)
+
+
+def registration(request):
+    context = {
+        'msg': 'registration html'
+    }
+    return render(request, 'registration.html', context)
+
+
+def dashboard(request):
+    context = {
+        'msg': 'dashboard html'
+    }
+    return render(request, 'dashboard.html', context)
+
+
+# design APIrest with djangorestframework here..
--- mauridb/urls.py
+++ mauridb/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from backend import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^registration/$', views.registration, name='registration'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
 ]
